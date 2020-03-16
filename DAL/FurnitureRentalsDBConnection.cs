3fa07c6e72c0d76cfd0160077f9175795351e370
Ashok J <ashok@swapstech.com>
2020-03-15 20:44:42 -0400

    Added DB Connection class to the repo

---
+++ DAL/FurnitureRentalsDBConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace FurnitureRentals.DAL
+{
+    /// <summary>
+    /// Class initialized the db connection string
+    /// </summary>
+    public static class FurnitureRentalsDBConnection
+    {
+        /// <summary>
+        /// Method that creates the DB connection to SQL Server
+        /// </summary>
+        /// <returns>Returns SQL connection</returns>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=RentMe;" +
+                "Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
