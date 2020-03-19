74692d8835874b1291624cf41bc083a88f648aae
Ashok Jekka Jothikrishnan <ajekkaj1@my.westga.edu>
2020-03-18 23:44:03 -0400

    Added tabbed pane to the main form

--- UserControl/ManageCustomerControl.cs
+++ UserControl/ManageCustomerControl.cs
@@ -2,120 +2,24 @@
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
-using TechSupport.Controller;
-using TechSupport.Model;
 
-namespace TechSupport.User_Controls
+namespace FurnitureRentals.User_Controls
 {
     /// <summary>
     /// Class adds a new incident to the in-memory database
     /// </summary>
-    public partial class AddIncidentUserControl : UserControl
-    {
-        private readonly CustomerController customerController;
-        private readonly ProductController productController;
-        private readonly IncidentController incidentController;
-        private readonly RegistrationController registrationController;
+    public partial class ManageCustomerUserControl : UserControl { 
 
         public event EventHandler DataSaved;
 
         /// <summary>
         /// Constructor initializes the class level variables
         /// </summary>
-        public AddIncidentUserControl()
+        public ManageCustomerUserControl()
         {
-            InitializeComponent();
-            this.incidentController = new IncidentController();
-            this.customerController = new CustomerController();
-            this.productController = new ProductController();
-            this.registrationController = new RegistrationController();
-
-            cbCustomer.DisplayMember = "Name";
-            cbCustomer.ValueMember = "CustomerID";
-
-            cbProduct.DisplayMember = "Name";
-            cbProduct.ValueMember = "ProductCode";
-
-            try
-            {
-                cbCustomer.DataSource = this.customerController.GetAllCustomers();
-                cbCustomer.SelectedIndex = 0;
-            }
-            catch(Exception) 
-            {
-                MessageBox.Show("Unable to load customers", "Error");
-            }
-
-            try
-            {
-                cbProduct.DataSource = this.productController.GetAllProducts();
-                cbProduct.SelectedIndex = 0;
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Unable to load products", "Error");
-            }
+            //InitializeComponent();
+            
         }
-
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-            cbCustomer.SelectedIndex = 0;
-            cbProduct.SelectedIndex = 0;
-            this.txtTitle.Text = "";
-            this.txtDescription.Text = "";
-            this.errorMessage.Text = "";
-        }
-
-        private void txtTitle_TextChanged(object sender, EventArgs e)
-        {
-            this.errorMessage.Text = "";
-        }
-
-        private void txtDescription_TextChanged(object sender, EventArgs e)
-        {
-            this.errorMessage.Text = "";
-        }
-
-        /// <summary>
-        /// Method that adds the incident to the incidents table
-        /// </summary>
-        private void btnCreateIncident_Click(object sender, EventArgs e)
-        {
-            var customerId = this.cbCustomer.SelectedValue.ToString();
-            var productCode = this.cbProduct.SelectedValue.ToString();
-            var title = this.txtTitle.Text;
-            var description = this.txtDescription.Text;
-
-            try
-            {
-                if(this.registrationController.IsRegistered(Convert.ToInt32(customerId), productCode))
-                {
-                    IncidentDB incident = new IncidentDB();
-                    incident.CustomerID = customerId;
-                    incident.ProductCode = productCode;
-                    incident.Title = title;
-                    incident.Description = description;
-                    incidentController.Add(incident);
-                    if (incident.IncidentID > 0)
-                    {
-                        MessageBox.Show("Incident is addedd successfully", "Success");
-
-                        if (DataSaved != null)
-                        {
-                            this.DataSaved(this, e);
-                        }
-                    }
-                }                
-                else
-                {
-                    MessageBox.Show("There is no registration associated with the product", "Error");
-                }
-            }
-            catch (ArgumentException ae)
-            {
-                this.errorMessage.ForeColor = Color.Red;
-                this.errorMessage.Text = ae.Message.ToString();
-            }
-        }
+        
     }
 }
--- View/MainForm.Designer.cs
+++ View/MainForm.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabManageCustomerUserControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabManageCustomerUserControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabManageCustomerUserControl
+            // 
+            this.tabManageCustomerUserControl.Controls.Add(this.tabPage1);
+            this.tabManageCustomerUserControl.Location = new System.Drawing.Point(12, 32);
+            this.tabManageCustomerUserControl.Name = "tabManageCustomerUserControl";
+            this.tabManageCustomerUserControl.SelectedIndex = 0;
+            this.tabManageCustomerUserControl.Size = new System.Drawing.Size(1089, 507);
+            this.tabManageCustomerUserControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1081, 478);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Manage Customer";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1113, 551);
+            this.Controls.Add(this.tabManageCustomerUserControl);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.tabManageCustomerUserControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabManageCustomerUserControl;
+        private System.Windows.Forms.TabPage tabPage1;
     }
 }
 
