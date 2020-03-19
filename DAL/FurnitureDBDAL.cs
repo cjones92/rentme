08fb6069b2ffa9506d35d1e2fbd2c2f6ad7a1c70
Chris Jones <cjones92@my.westga.edu>
2020-03-19 14:13:12 -0400

    Added model folder and model furniture class

--- DAL/FurnitureDBDAL.cs
+++ DAL/FurnitureDBDAL.cs
@@ -11,6 +11,50 @@ namespace FurnitureRentals.DAL
     /// </summary>
     public static class FurnitureDBDAL
     {
-       
+       public List<Furniture> GetFurnitureBySerialNumber(string serialNumber)
+        {
+
+            Incident chosenIncident = new Incident();
+            chosenIncident.IncidentID = incidentID;
+
+            string selectStatement = "SELECT serial_no AS 'Serial Number', furniture.description AS Item, furniture_style.description AS Style, total_available AS 'Total Available' " +
+"FROM furniture JOIN furniture_style ON furniture.style_id = furniture_style.style_id JOIN inventory ON furniture.furniture_id = inventory.furniture_id WHERE furniture.";
+            ;
+
+            using (SqlConnection connection = FurnitureRentalsDBConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("@IncidentID", chosenIncident.IncidentID);
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+
+
+                        while (reader.Read())
+                        {
+
+                            chosenIncident.ProductCode = reader["ProductCode"].ToString();
+                            chosenIncident.DateOpened = (DateTime)reader["DateOpened"];
+                            chosenIncident.CustomerName = reader["CustomerName"].ToString();
+                            chosenIncident.TechnicianName = reader["TechnicianName"].ToString();
+                            chosenIncident.Title = reader["Title"].ToString();
+                            chosenIncident.Description = reader["Description"].ToString();
+
+                            if (reader["DateClosed"] != DBNull.Value)
+                            {
+                                chosenIncident.DateClosed = (DateTime)reader["DateClosed"];
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+
+            return chosenIncident;
+        }
     }
 }
