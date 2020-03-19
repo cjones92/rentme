bdf4b1bc41f4004532376d892d50e423ceee9c78
Chris Jones <cjones92@my.westga.edu>
2020-03-19 13:55:50 -0400

    Created FurnitureDBDAL class

---
+++ DAL/FurnitureDBDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace FurnitureRentals.DAL
+{
+    /// <summary>
+    /// Class initialized the db connection string
+    /// </summary>
+    public static class FurnitureDBDAL
+    {
+       
+    }
+}
