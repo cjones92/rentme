0c4c14f94be0d0bf2fa73ae437b367f3e56ec3fe
Chris Jones <cjones92@my.westga.edu>
2020-03-21 11:21:17 -0400

    Added category DAL and model classes

---
+++ DAL/CategoryDBDAL.cs
@@ -0,0 +1,50 @@
+﻿using FurnitureRentals.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureRentals.DAL
+{
+    class CategoryDBDAL
+    {
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+
+
+            string selectStatement =
+                "SELECT category_id AS CategoryID, description as Description FROM furniture_category";
+
+            using (SqlConnection connection = FurnitureRentalsDBConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            Category category = new Category();
+                            category.CategoryID = (int)reader["CategoryID"];
+                            category.CategoryDescription = reader["Description"].ToString();
+
+
+
+                            categories.Add(category);
+                        }
+
+                    }
+
+                }
+            }
+
+
+            return categories;
+        }
+    }
+}
