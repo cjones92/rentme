90fb6fa8daf3276faf28692e1c11bb8c92571f64
Chris Jones <cjones92@my.westga.edu>
2020-03-21 11:24:38 -0400

    Added style model and DAL classes

---
+++ DAL/StyleDBDAL.cs
@@ -0,0 +1,50 @@
+﻿using FurnitureRentals.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureRentals.DAL
+{
+    class StyleDBDAL
+    {
+        public List<Style> GetStyles()
+        {
+            List<Style> styles = new List<Style>();
+
+
+            string selectStatement =
+                "SELECT style_id AS StyleID, description as Description FROM furniture_style";
+
+            using (SqlConnection connection = FurnitureRentalsDBConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            Style style = new Style();
+                            style.StyleID = (int)reader["StyleID"];
+                            style.StyleDescription = reader["Description"].ToString();
+
+
+
+                            styles.Add(style);
+                        }
+
+                    }
+
+                }
+            }
+
+
+            return styles;
+        }
+    }
+}
---
+++ Model/Style.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureRentals.Model
+{
+    class Style
+    {
+        public int StyleID { get; set; }
+
+        public string StyleDescription { get; set; }
+    }
+}
