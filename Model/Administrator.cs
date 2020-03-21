c296ab72fa7c45b8f7e708660e2bc22ac21a5340
Chris Jones <cjones92@my.westga.edu>
2020-03-21 14:41:53 -0400

    Added Administrator and Employee model classes

---
+++ Model/Administrator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureRentals.Model
+{
+    class Administrator
+    {
+        public int AdminstratorID {get; set;}
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Sex { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+        
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+
+        }
+    }
+
